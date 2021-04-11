c2db741fa5e394e047f4c9192a783629e2d92a87
Alex Vasilica <alex.vasilica14@gmail.com>
2021-04-11 21:54:38 +0300

    Search-page

--- Views/Search.php
+++ Views/Search.php
@@ -19,16 +19,25 @@
   ?>
   <main>
     <div class="page-content-search flex-all" style="align-items: flex-start; flex-wrap:wrap;"> 
-        <div class="left-side" style="width: 300px; height: 100vh; background-color: black;"> 
+        <div class="left-side" style="width: 300px; height: 100vh; background-color: #903749;"> 
+            <p style="color: #2B2E4A; font-size: 30px; padding-left: 10px;">Filter</p>
+            <p style="color: white; font-size: 15px; padding-left: 10px;">- State</p>
+            <p style="color: white; font-size: 15px; padding-left: 40px;">- County</p>
+            <p style="color: white; font-size: 15px; padding-left: 40px;">- City</p>
+            <p style="color: white; font-size: 15px; padding-left: 10px;">+ Severity</p>
+            <p style="color: white; font-size: 15px; padding-left: 10px;">+ Temperature</p>
+            <p style="color: white; font-size: 15px; padding-left: 10px;">+ Airport Code</p>
+            <p style="color: white; font-size: 15px; padding-left: 10px;">+ Side</p>
+
         </div>
-        <div class="right-side flex-all" style="flex:1; flex-wrap:wrap; justfiy-content:space-between;">
+        <div class="right-side flex-all" style="flex:1; flex-wrap:wrap; justfiy-content:space-between; background-color: #2B2E4A;">
 
         <?php
             for($i = 0; $i < 30; $i++) {
                 ?>
 
-            <div  style="width: 300px; min-height: 120px; max-width: 100%; padding: 30px; background-color: red; margin: 10px;">
-                Test 
+            <div  class="flex-all box-shadow-re-use" style="width: 300px; min-height: 120px; max-width: 100%; padding: 30px; background-color: #903749; margin: 10px; color: white;">
+                <p style="text-align: center;"> Accident on Brice Rd at Tussing Rd. Expect delays. </p>
             </div>
 
                 <?php
