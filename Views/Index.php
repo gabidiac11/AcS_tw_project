727506bfd8616177983a975a5a191d3c1ea381a6
Gabi <gabidiac11@gmail.com>
2021-04-11 21:02:37 +0300

    #33 Make header highlight based on page; #34 Make the home page list of items

--- Views/Index.php
+++ Views/Index.php
@@ -23,7 +23,7 @@
             while ($i < 20) {
             ?>
 
-                <a href="/accidents/<?= ($i) ?>" class="home-page-item box-shadow-re-use overwrite-achor<?= $i == 2 || $i == 4  ? " item-right" : "" ?>">
+                <a href="/accidents/<?= ($i) ?>" class="home-page-item box-shadow-re-use overwrite-achor<?= $i % 2 == 0 ? " item-right" : "" ?>">
                     <img src="./../assets/images/not-found.jpg" />
                     <div class="content-item">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
