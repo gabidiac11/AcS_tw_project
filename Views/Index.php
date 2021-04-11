166f9874471607798304e70bce5b0cfc15a73461
Gabi <gabidiac11@gmail.com>
2021-04-11 21:02:21 +0300

    #33 Make header highlight based on page; #34 Make the home page list of items

--- Views/Index.php
+++ Views/Index.php
@@ -2,28 +2,42 @@
 <html lang="en">
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, minimal-ui" />
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <link rel="icon" type="image/png" href="./../assets/favicon/logo.ico" />
-  
-  <title> Welcome - USA Accidents Smart Visualizer</title>
-  <link href="../../assets/css/index.css" media="all" rel="stylesheet" type="text/css" />
-  <link href="../../assets/css/header.css" media="all" rel="stylesheet" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, minimal-ui" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="icon" type="image/png" href="./../assets/favicon/logo.ico" />
+
+    <title> Welcome - USA Accidents Smart Visualizer</title>
+    <link href="../../assets/css/index.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/header.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/home.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-  <?php
-  require_once __DIR__ . '/Layout/Header.php';
-  ?>
-  <main>
-    <div class="image-presentation">
-      <img class="box-shadow-re-use" src="./../assets/images/not-found.jpg" />
-    </div>
-    <div class="page-content">
-      <h1> Home </h1>
-      <p class="text-box-shadow-light-re-use">Welcome. </p>
-   </div>
-  </main>
+    <?php
+    require_once __DIR__ . '/Layout/Header.php';
+    ?>
+    <main id="home-page">
+        <div class="page-content">
+            <?php
+            $i = 1;
+            while ($i < 20) {
+            ?>
+
+                <a href="/accidents/<?= ($i) ?>" class="home-page-item box-shadow-re-use overwrite-achor<?= $i == 2 || $i == 4  ? " item-right" : "" ?>">
+                    <img src="./../assets/images/not-found.jpg" />
+                    <div class="content-item">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </div>
+                </a>
+
+
+            <?php
+                $i++;
+            }
+            ?>
+
+        </div>
+    </main>
 </body>
 
 </html>
--- Views/Layout/Header.php
+++ Views/Layout/Header.php
@@ -3,11 +3,27 @@
         <img src="./../../assets/svg/logo.svg">
         <div class="brand-title text-box-shadow-re-use">AcS</div>
     </a>
-    <ul class="flex-all flex-end header-meniu-links">
+
+    <ul id="list-header" class="flex-all flex-end header-meniu-links">
         <li> <a href="/about" class="text-box-shadow-re-use"> About </a> </li>
         <li> <a href="/search" class="text-box-shadow-re-use"> Search </a> </li>
         <li> <a href="/charts" class="text-box-shadow-re-use"> Charts </a> </li>
         <li> <a href="/maps" class="text-box-shadow-re-use"> Maps </a> </li>
         <li> <a href="/contact" class="text-box-shadow-re-use"> Contact </a> </li>
     </ul>
+    <script>
+        /**
+         * highlight the current page
+         */
+        (() => {
+            const headerLiElements = document.querySelectorAll("#list-header a");
+
+            for (let i = 0; i < headerLiElements.length; i++) {
+                if (window.location.pathname.indexOf(headerLiElements[i].getAttribute("href")) === 0) {
+                    headerLiElements[i].classList.add("selected");
+                    break;
+                }
+            }
+        })()
+    </script>
 </header>
