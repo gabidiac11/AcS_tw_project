4d505676db69cd881236b03c5bf2b89c79a3f6c9
gradinariuflorin-marian <mariangrd11@gmail.com>
2021-05-19 01:30:22 +0300

    Added Admin javascript and Adminpanel controller

---
+++ assets/js/login.js
@@ -0,0 +1,29 @@
+function loadStorage() {
+    if (localStorage.getItem("Check") !== null && localStorage.getItem("Check") === "1") {
+        var username = document.getElementById("name");
+        var password = document.getElementById("pass");
+        var check = document.getElementById("remember_me");
+        document.getElementById("name").innerHTML= "text";
+        username.value = localStorage.getItem("AdminUser");
+        password.value = localStorage.getItem("AdminPass");
+        document.getElementById("remember_me").checked = true;
+    }
+}
+function validate(){
+    var username = document.getElementById("name").value;
+    var password = document.getElementById("pass").value;
+    if ( username == "test" && password == "test"){
+        rememberMe();
+        window.open('/AdminPanel', '_blank')
+    }
+}
+function rememberMe(){
+    var check = document.getElementById("remember_me");
+    if(check.checked){
+        var username = document.getElementById("name").value;
+        var password = document.getElementById("pass").value;
+        localStorage.setItem("AdminUser", username);
+        localStorage.setItem("AdminPass", password);
+        localStorage.setItem("Check", "1");
+    }
+}
