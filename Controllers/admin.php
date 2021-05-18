d37d7ce068ba87c6ddd62850aa2876d814e1ea45
gradinariuflorin-marian <mariangrd11@gmail.com>
2021-05-18 13:25:53 +0300

    Added Admin controller and admin page.

---
+++ Controllers/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+class admin extends Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->loadView("admin", []);
+    }
+}
---
+++ Views/Admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, minimal-ui" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="icon" type="image/png" href="./../assets/favicon/logo.ico" />
+
+    <title> About - USA Accidents Smart Visualizer</title>
+    <link href="../../assets/css/index.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/header.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/about.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<?php
+require_once __DIR__ . '/Layout/Header.php';
+?>
+<main id="admin-page">
+
+</main>
+</body>
+
+</html>
